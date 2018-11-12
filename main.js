6834bf0a53e7f9cb6b95815d0dc9f3ba2eefb763
Hakerh400 <hakerh403@gmail.com>
2018-11-12 23:34:47 +0100

    commit

--- main.js
+++ main.js
@@ -58,8 +58,6 @@ function onInput(buf){
 }
 
 async function onReq(req, res){
-  console.log('Got request...');
-
   req.on('error', console.log);
 
   if(!ready) return err('The server is not ready yet');
